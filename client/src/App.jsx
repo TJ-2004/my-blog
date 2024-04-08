80b5d462bb6bc77fb6d6244e0a54b78d3266b40a
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-04-08 21:46:07 +0530

    Make Dashboard Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home, About, SignIn, SignUp, Dashboard, Projects } from './pages';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,7 +12,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
