0b6938e636c028742f8beb293841910d883cc384
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-04-14 16:26:20 +0530

    Add Post to DashBoard

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,10 @@
+
+
+export default function DashPosts() {
+  return (
+    <div>
+    DashPosts
+      
+    </div>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import DashSidebar from "../components/DashSidebar"
+import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 const Dashboard = () => {
   const location = useLocation();
@@ -17,11 +18,12 @@ const Dashboard = () => {
     // console.log(tabFromUrl);
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
         <DashSidebar />
       </div>
       {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
     </div>
   );
 };
