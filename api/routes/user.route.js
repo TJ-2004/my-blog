28616bfab3511c9d7b3b129040904b047d062c79
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-04-20 17:27:48 +0530

    Create getUser Api Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUsers,
   signout
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
@@ -13,4 +14,5 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post("/signout",signout);
+router.get("/getusers",verifyToken,getUsers);
 export default router;
