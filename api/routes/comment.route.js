10f7a97b6b37c84234d231f721de5842023470a7
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-07-31 22:09:44 +0530

    Add Edit Functionality

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,9 +4,14 @@ import { verifyToken } from '../utils/verifyUser.js';
 import { createComment } from '../controllers/comment.controller.js';
 import {getPostComments} from '../controllers/comment.controller.js';
 import { likeComment } from '../controllers/comment.controller.js';
+import { editComment } from '../controllers/comment.controller.js';
+
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId',getPostComments);
 router.put('/likeComment/:commentId',verifyToken,likeComment);
+router.put('/editComment/:commentId',verifyToken,editComment);
+
+
 export default router;
